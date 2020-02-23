f3b2b28bb5556dae69c178d91874ac19aef2389f
Nathan Oh <nathanoh93@gmail.com>
2020-02-23 19:46:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {artist: 'song'}
 function updatePlaylist (playlist, artistName, songTitle) {
-  return Object.assign ({}, playlist, {[artistName]: songTitle})
+  playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
