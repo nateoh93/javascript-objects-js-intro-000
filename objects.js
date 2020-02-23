62736dddf14be1a800db16c829e3864e51e2c515
Nathan Oh <nathanoh93@gmail.com>
2020-02-23 19:49:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
-var playlist = {artist: 'song'}
+var playlist = {artistName: 'songTitle'}
+
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist.artistName
   return playlist
 }
-delete playlist.artistName
